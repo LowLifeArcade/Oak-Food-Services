7cd378815d14bedbf3cebf6443470294b92a7401
LowLifeArcade <68655112+LowLifeArcade@users.noreply.github.com>
2021-03-24 16:08:32 -0700

    adding back mobile menu

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -707,7 +707,7 @@ const Layout = ({ children }) => {
     <React.Fragment>
       {head()} 
       {nav()} 
-      {/* {accordian()}  */}
+      {accordian()} 
       {/* {sideMenu()}  */}
       {/* {sideBar()} */}
       {' '}
